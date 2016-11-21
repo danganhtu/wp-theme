e226eb894c29cdcee263512b6bff6aed3ae98d53
d_tu@stagegroup.jp <d_tu@stagegroup.jp>
2016-11-21 16:51:08 +0700

    Functions

--- wp-content/themes/thachpham/functions.php
+++ wp-content/themes/thachpham/functions.php
@@ -11,7 +11,7 @@ define('CORE', THEME_URL."/core");
 @ Nhung fine /core/init.php
 **/
 
-require_once(CORE."/inut.php");
+require_once( CORE . "/init.php" );
 
 /** 
 @ Thiet lap chieu rong noi dung
@@ -26,15 +26,49 @@ if (!isset($content_width)){
 **/
 if (!function_exists('thachpham_theme_setup')){
 	function thachpham_theme_setup(){
-		/* thiet lap textdomain*/
-		$language_folder = THEME_URL.'/languages';
-		load_theme_textdomain('thachpham',$language_folder);
-		/*tu dong them link ASS len <head>*/
 
-		/**add_theme_support('automatic-feed-links');**/
+		/** thiet lap textdomain **/
+		$language_folder = THEME_URL . '/languages';
+		load_theme_textdomain('thachpham', $language_folder);
+		
+		/** Tu dong them RSS link len the <head> **/
+		add_theme_support( 'automatic-feed-links'); 
 
-		/***Them post thumbnail**/
-		/**add_theme_support**/
+		/**Theme post thumbnail**/
+		add_theme_support('post-thumbnails');
+
+		/** Post format */
+		add_theme_support('post-formats', array(
+			'image',
+			'video',
+			'gallery',
+			'quote',
+			'link'
+			));
+
+		/** Them title-tag  */
+		add_theme_support('title-tag');
+
+		/**Theme custom background **/
+		$default_background = array(
+			'default-color' => '#e8e8e8'
+			);
+		add_theme_support('custom-background',$default_background);
+
+		/**Theme menu*/
+		register_nav_menu('primary-menu',__('Primary Menu','thachpham'));
+
+		/**Tao sidebar */
+		$sidebar = array(
+			'name' => __('Main Sidebar','thachpham'),
+			'id' => 'main-sidebar', 
+			'description' => __('Default sidebar'),
+			'class' => 'main-sidebar',  
+			'before_title' => '<h3 class="widgettitle">',  
+			'after_title' => '</h3>'
+			);
+		register_sidebar($sidebar);
+
 	}
 	add_action('init','thachpham_theme_setup');
 }
--- wp-content/themes/thachpham/header.php
+++ wp-content/themes/thachpham/header.php
@@ -1,1 +1,13 @@
-<?php get_header(); ?>
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset'); ?>" />
+<link rel="profile" href="http://gmgp.org/xfn/11" />
+<lin
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
